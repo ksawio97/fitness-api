b7656b5e980738de8b1d80c0f04a17d2e2c95958
Ksawery Kuczynski <ksawerykuczynskikk@gmail.com>
2024-04-02 13:50:36 +0200

    Added corsConfigurer for allowing different request origins

--- src/main/java/com/ksawio/fitnessapi/FitnessApiApplication.java
+++ src/main/java/com/ksawio/fitnessapi/FitnessApiApplication.java
@@ -1,10 +1,27 @@
 package com.ksawio.fitnessapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FitnessApiApplication {
+    @Value("#{'${custom.security.allowed_origins}'.split(',')}")
+    private String[] allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("NullableProblems") CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FitnessApiApplication.class, args);
